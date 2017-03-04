2341ab5d90c34be697a1300184ba7971c6eebc68
Peter Herman <peter.b.herman@gmail.com>
2017-03-04 16:35:31 -0600

    added basic python assignments

---
+++ methodtest.py
@@ -0,0 +1,20 @@
+# Gators and Monkeys
+str = "If monkeys like bananas, then I must be a monkey!"
+print str.find('monkey')
+newstr = str.replace('monkey', 'alligator')
+print newstr
+
+# min and max
+x = [2,54,-2,7,12,98]
+print min(x)
+print max(x)
+
+# first and last
+y = ["hello",2,54,-2,7,12,98,"world"]
+print y[0]
+print y[-1]
+z = [y[0], y[-1]]
+print z
+
+# newlist
+x = [19,2,54,-2,7,12,98,32,10,-3,6]
---
+++ pythontest.py
@@ -0,0 +1,14 @@
+myVar = "python"
+print myVar
+myVar = "ME"
+print myVar
+print 2+2
+print "string1" + "string2"
+print "string3", 1
+firstname = "peter"
+lastname = "herman"
+print "my name is {} {}".format(firstname, lastname)
+x = [7, "potato", 4, "aardvark", 9, "zillow"]
+x.sort()
+print x
+print x[1]
