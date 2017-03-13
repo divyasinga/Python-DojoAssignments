a0dbbcb3a07b8cbb4f2e67fd77044735342c82f1
Peter Herman <peter.b.herman@gmail.com>
2017-03-13 10:45:39 -0500

    added finished wall project

--- Flask_MySQL/The_Wall/server.py
+++ Flask_MySQL/The_Wall/server.py
@@ -1,11 +1,13 @@
 from flask import Flask, session, request, render_template, redirect, flash
 from mysqlconnection import MySQLConnector
+from flask_bcrypt import Bcrypt
 import re
 
 app = Flask(__name__)
 mysql = MySQLConnector(app, 'walldb')
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 app.secret_key = "WatchersOnTheWall"
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def index():
@@ -61,21 +63,18 @@ def register():
         query_data = { 'email': email }
         user = mysql.query_db(user_query, query_data)
         session['id'] = user[0]['id']
-        print session['id']
-
         return redirect('/wall')
 
 @app.route('/wall')
 def wall():
     if 'user' in session:
-        query = "SELECT messages.message, messages.created_at, users.first_name, users.last_name, messages.id FROM messages JOIN users on (users.id = messages.user_id)"
-        # comments.comment_text/JOIN comments on (comments.message_id = messages.id)
+        query = "SELECT messages.message, messages.created_at, users.first_name, users.last_name, messages.id, messages.user_id FROM messages JOIN users on (users.id = messages.user_id) ORDER by created_at DESC"
         messages = mysql.query_db(query)
-        query2 = "SELECT comments.comment_text, comments.created_at, users.first_name, users.last_name, comments.message_id, messages.id FROM comments JOIN messages on (comments.message_id = messages.id) JOIN users on (comments.user_id = users.id)"
+        query2 = "SELECT comments.comment_text, comments.created_at, users.first_name, users.last_name, comments.message_id, messages.id FROM comments JOIN messages on (comments.message_id = messages.id) JOIN users on (comments.user_id = users.id) ORDER by created_at DESC"
         comments = mysql.query_db(query2)
-        print comments
-        print '++++++++++++++++++++++++++++++++++++++'
-        return render_template('wall.html', messages=messages, comments=comments)
+        deletequery = "SELECT users.id, messages.user_id FROM messages JOIN users on (users.id = messages.user_id)"
+        deletes = mysql.query_db(deletequery)
+        return render_template('wall.html', messages=messages, comments=comments, deletes=deletes, user=session['id'])
     else:
         return redirect('/')
 
@@ -89,24 +88,47 @@ def post():
     posttext = request.form['posttext']
     userid = session['id']
     print posttext
-    query = 'INSERT INTO messages (message, user_id, created_at, updated_at) VALUES (:message, :user_id, NOW(), NOW())'
-    data = {
-        'message': posttext,
-        'user_id': userid
-    }
-    mysql.query_db(query, data)
-    return redirect('/wall')
+    if len(posttext) > 0:
+        query = 'INSERT INTO messages (message, user_id, created_at, updated_at) VALUES (:message, :user_id, NOW(), NOW())'
+        data = {
+            'message': posttext,
+            'user_id': userid
+        }
+        mysql.query_db(query, data)
+        return redirect('/wall')
+    else:
+        flash('You have to type something to post.', 'posterror')
+        return redirect('/wall')
 
 @app.route('/comment/<postid>', methods=['POST'])
 def comment(postid):
     commenttext = request.form['commenttext']
     userid = session['id']
-    print commenttext
-    query = 'INSERT INTO comments (comment_text, user_id, message_id, created_at, updated_at) VALUES (:message, :user_id, :message_id, NOW(), NOW())'
+    if len(commenttext) > 0:
+        query = 'INSERT INTO comments (comment_text, user_id, message_id, created_at, updated_at) VALUES (:message, :user_id, :message_id, NOW(), NOW())'
+        data = {
+            'message': commenttext,
+            'user_id': userid,
+            'message_id': postid
+        }
+        mysql.query_db(query, data)
+        return redirect('/wall')
+    else:
+        flash('You have to type something to comment.', 'commenterror')
+        return redirect('/wall')
+
+@app.route('/delete/<postid>', methods=['POST'])
+def delete(postid):
+    # if statement to confirm user is in session
+    deletepostcomments = 'DELETE from comments WHERE message_id = :id'
     data = {
-        'message': commenttext,
-        'user_id': userid,
-        'message_id': postid
+        'id': postid
+    }
+    mysql.query_db(deletepostcomments, data)
+
+    query = 'DELETE from messages WHERE id = :id'
+    data = {
+        'id': postid
     }
     mysql.query_db(query, data)
     return redirect('/wall')
